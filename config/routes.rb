99b4273f34c85f264485d80b2b910ba0eff24f1c
Evgeniy Lavretskiy <e.lavretskiy@gmail.com>
2015-06-08 21:50:32 +0300

    Исправления по хаунду

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ News::Engine.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
   resources :articles do
-    get '/admin' => 'articles#admin', :as => :admin, on: :collection
+    get '/admin' => 'articles#admin', as: :admin, on: :collection
   end
 end
